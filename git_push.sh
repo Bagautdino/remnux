41a2ed21dd6975e0a7433eb0c3fd52a7bc007b5f
Bagautdino <sir.aivaz21@gmail.com>
2022-11-01 14:45:08 +0300

    AAAAAAAA

--- git_push.sh
+++ git_push.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+echo "Your generation Code: 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"
+sleep 5
 git init
 git status
 git add .
 git status
-git commit -m "Last commit"
+git commit -m "$1"
 git status
 git branch -M main
 git remote add origin https://github.com/Bagautdino/remnux.git
